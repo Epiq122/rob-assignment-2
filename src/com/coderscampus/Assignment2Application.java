88ec9bddafd79794edcaccdebe4ca797946ae1aa
Robert Gleason <l2ob122@gmail.com>
2022-08-28 16:23:54 -0700

    finished making the game loop and added the guessing functionality

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -6,31 +6,39 @@ import java.util.Scanner;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		
-	
+
 		Random random = new Random();
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Guess a Number between 1 and 100");
-		int guessNumber = scan.nextInt();
-		
-		
-		int guesses = 5;
-		int max = 100;
-		int min = 1;
-		int showRandom = min + random.nextInt(max);
-		System.out.println(showRandom);
-		
-		
-		
-		if(guessNumber  == showRandom) {
-			System.out.println("Good Guess!");
-		}else {
-			System.out.println("Try Again");
-			guesses-= 1;
-			System.out.println(guesses);
+
+		int randomNumber = random.nextInt(100) + 1;
+		int guessesRemaining = 5;
+
+		while (true) {
+			System.out.println("Guess a number between 1 and 100: ");
+
+			int guessNumber = scan.nextInt();
+
+			if (guessesRemaining == 0) {
+				System.out.println("Sorry You Lost the number was " + randomNumber);
+
+				break;
+			} else if (guessNumber > randomNumber) {
+				System.out.println("The Number you have entered is too high please try again");
+				guessesRemaining--;
+				System.out.println("You have " + guessesRemaining + " remaining.");
+
+			} else if (guessNumber < randomNumber) {
+				System.out.println("The Number you have entered is too low please try again");
+				guessesRemaining--;
+				System.out.println("You have " + guessesRemaining + " remaining.");
+
+			} else if (guessNumber == randomNumber) {
+				System.out.println("Congrats the number was " + randomNumber);
+				break;
+			}
+
 		}
-	
 
+		scan.close();
+	}
 }
-}
-
